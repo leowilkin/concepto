d9852977da9ca7bed0c943f5ab6715c1409b2cac
Leo Wilkin <64415791+leowilkin@users.noreply.github.com>
2023-11-11 20:29:28 +0000

    add concepto

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Concepto",
+  description: "Draw & create your next app using the power of AI",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"></link>
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
